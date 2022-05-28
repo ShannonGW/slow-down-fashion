e2082449834d10b9a8d3d4d1641af57ccbe8feaf
ShannonGW <ShannonWilliams93@gmail.com>
2022-05-28 18:42:39 -0400

    add success message to suggestions input form

--- client/src/components/SuggestionsForm.js
+++ client/src/components/SuggestionsForm.js
@@ -1,9 +1,31 @@
 import React, { useState } from "react";
 import "../App.css";
-import { useState } from "react";
 
 export default function SuggestionsForm() {
-  const [formData, setFormData] = useState({});
+  const [values, setValues] = useState({
+    brandName: "",
+    brandWebsite: "",
+    brandInfo: "",
+  });
+
+  const [submited, setSubmitted] = useState(false);
+
+  const handleBrandNameInputChange = (event) => {
+    setValues({ ...values, brandName: event.target.value });
+  };
+
+  const handleBrandWebsiteInputChange = (event) => {
+    setValues({ ...values, brandWebsite: event.target.value });
+  };
+
+  const handleBrandInfoInputChange = (event) => {
+    setValues({ ...values, brandInfo: event.target.value });
+  };
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+    setSubmitted(true);
+  };
 
   return (
     <div class="suggestions-form-container">
@@ -13,31 +35,42 @@ export default function SuggestionsForm() {
         clothing brands. Use this form to your add favorite brand, and let's
         make a difference.{" "}
       </h5>
-      <form class="suggestions-form">
-        {/* <div class="success-message">Success! Thank you for suggestion</div> */}
+      <form class="suggestions-form" onSubmit={handleSubmitForm}>
+        {submited ? (
+          <div className="success-message">
+            {" "}
+            Success! Thanks for your suggestion
+          </div>
+        ) : null}
         <input
+          onChange={handleBrandNameInputChange}
+          value={values.brandName}
           id="brand-name"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Brand Name"
           name="brandName"
         />
         <input
+          onChange={handleBrandWebsiteInputChange}
+          value={values.brandWebsite}
           id="brand-website"
-          class="form-field"
-          type="url"
+          className="form-field"
+          type="text"
           placeholder="Website"
-          name="brandWebsite "
+          name="brandWebsite"
         />
         <input
+          onChange={handleBrandInfoInputChange}
+          value={values.brandInfo}
           id="brand-info"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Tell us about them!"
-          name="brand-info"
+          name="brandInfo"
         />
 
-        <button class="form-field" type="submit">
+        <button className="form-field" type="submit">
           Submit
         </button>
       </form>
